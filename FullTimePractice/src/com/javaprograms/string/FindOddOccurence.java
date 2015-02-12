fe40050ec4c39739bbfc9299a3f758ca61b3038e
sa3047 <swapnil_aher@yahoo.co.in>
2015-02-11 20:24:49 -0500

    commit

--- FullTimePractice/src/com/javaprograms/string/FindOddOccurence.java
+++ FullTimePractice/src/com/javaprograms/string/FindOddOccurence.java
@@ -1,17 +1,30 @@
 package com.javaprograms.string;
 
+/*
+ * AUTHOR : Swapnil Aher
+ * Program : To find the odd occurence of a Number in Array. 
+ * Logic: Use ^(exclusive-OR) operation on each item, you will get the Odd occurring number
+ * Time complexity =O(n)
+ */
 public class FindOddOccurence {
 	
+	//
 	int returnOddOccurence(int[] input)
 	{
 		int result =0;
 		
+		for(int i=0;i<input.length;i++)
+		{
+			result = result ^ input[i];
+		}
 		return result;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int[] input = {1,2,3,2,1,3,3};
+	FindOddOccurence obj = new FindOddOccurence();
+	System.out.println("odd occurring number "+	obj.returnOddOccurence(input));
 	}
 
 }
