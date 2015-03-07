c30b34485c2164cc900db553ef98a9eedafd1bb0
sa3047 <swapnil_aher@yahoo.co.in>
2015-03-07 12:10:39 -0500

    initial commit

---
+++ FullTimePractice/src/com/java/trees/BuildTree.java
@@ -0,0 +1,22 @@
+package com.java.trees;
+/*@author : Swapnil Aher
+ *@date: 3/7/2015
+ *@purpose : Build a tree using given preorder and inorder traversal 
+ */
+
+public class BuildTree {
+
+	public static void main(String[] args)
+	{
+		  char in[] = {'D', 'B', 'E', 'A', 'F', 'C'};
+		  char pre[] = {'A', 'B', 'D', 'E', 'C', 'F'};
+		  
+		  buildTree(in,pre);
+	}
+	
+	static TreeNode buildTree(char[] inorder,char[] preorder)
+	{
+		
+		return null;
+	}
+}
