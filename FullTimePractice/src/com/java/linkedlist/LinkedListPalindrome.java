4a0c1d1d5cd1aa5cf1d0a95046062ee29b133daf
sa3047 <swapnil_aher@yahoo.co.in>
2015-03-09 20:30:43 -0400

    Initial commit

---
+++ FullTimePractice/src/com/java/linkedlist/LinkedListPalindrome.java
@@ -0,0 +1,13 @@
+package com.java.linkedlist;
+/*
+ * @author : Swapnil Aher
+ * @date : 3/9/2015
+ * */
+public class LinkedListPalindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
