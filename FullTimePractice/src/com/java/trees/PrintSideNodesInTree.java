1710d8b27fb4d0bc98e3c6044a852c0613e266ca
sa3047 <swapnil_aher@yahoo.co.in>
2015-03-11 19:29:10 -0400

    test case 1 tested

--- FullTimePractice/src/com/java/trees/PrintSideNodesInTree.java
+++ FullTimePractice/src/com/java/trees/PrintSideNodesInTree.java
@@ -6,9 +6,83 @@ package com.java.trees;
  * */
 public class PrintSideNodesInTree {
 
+	//root reference for backtracking
+	static Node statRefRoot = null;
+	static boolean left = false;
+	static boolean right = false;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		/*  Create following Binary Tree
+        
+		    1) Test case 1
+		    
+			         12
+			      /  	 \
+			     8    	  4
+			    / \  	 / \
+			   3   5 	2   2
+			  /    \   /     \
+			 2      5 3       6 
+		     
+		 */
+			
+			Node root = new Node(12);
+			root.leftNode= new Node(8);
+			root.rightNode = new Node(4);
+			
+			root.leftNode.leftNode= new Node(3); //
+			root.leftNode.rightNode = new Node(5); 
+			
+			root.rightNode.leftNode = new Node(2); //
+			root.rightNode.rightNode =new Node(2);
+			
+			root.leftNode.rightNode.rightNode = new Node(5);//
+			root.rightNode.leftNode.leftNode =new Node(3);//
+			
+			//root.leftNode.leftNode.leftNode = new Node(2);
+			//root.rightNode.rightNode.rightNode = new Node(6);
+			
+			statRefRoot= root;
+			left=true;
+			
+			mPrintNodes(root);
+			
 	}
-
+	
+    static void mPrintNodes(Node node)
+    {
+    	
+    	
+    	if(left)
+    	{
+	    	if (node != null)
+	    	{
+	    		System.out.println(node.data);
+	    		mPrintNodes(node.leftNode);
+	    	}
+	    	else
+	    	{
+	    		//mPrintNodes(node.leftNode);
+	    		left = false;
+	    	}
+	    		
+    	}
+    	
+        if (node == statRefRoot || right==true)
+    	{
+    		if(node == statRefRoot)
+        	{
+    			right=true;
+        	}
+    		
+        	if(node != null)
+        	{
+        		System.out.println(node.data);
+        		mPrintNodes(node.rightNode);
+        	}
+    	}
+    	
+    }
 }
