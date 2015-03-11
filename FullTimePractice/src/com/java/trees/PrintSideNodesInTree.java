3f6fb2b5ece49d44981e2bbec48ab85e959c62d2
sa3047 <swapnil_aher@yahoo.co.in>
2015-03-11 18:43:06 -0400

    initial commit

---
+++ FullTimePractice/src/com/java/trees/PrintSideNodesInTree.java
@@ -0,0 +1,14 @@
+package com.java.trees;
+/*
+ *@author: Swapnil Aher
+ *@date: 3/11/2015
+ *@purpose: To print the elements which are along the side of the tree 
+ * */
+public class PrintSideNodesInTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
