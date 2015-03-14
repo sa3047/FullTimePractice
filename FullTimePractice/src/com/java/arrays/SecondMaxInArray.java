804d94619e810969eed9b1c339ef034990e796fe
sa3047 <swapnil_aher@yahoo.co.in>
2015-03-14 13:43:54 -0400

    tested for negative integer array

--- FullTimePractice/src/com/java/arrays/SecondMaxInArray.java
+++ FullTimePractice/src/com/java/arrays/SecondMaxInArray.java
@@ -2,11 +2,12 @@ package com.java.arrays;
 /*
  * @author: Swapnil Aher
  * @purpose: Find 2n highest element from the Array
+ * @checked for positive and negative integers
  * */
 
 public class SecondMaxInArray {
-	static int _1stMaxNum =0;
-	static int _2ndMaxNum =0;
+	static int _1stMaxNum =Integer.MIN_VALUE;
+	static int _2ndMaxNum =Integer.MIN_VALUE;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,7 +15,7 @@ public class SecondMaxInArray {
 		//int[] arr = {6,5,8,9,1,3};
 		
 		//test case 2
-		int[] arr = {12, 13, 1, 10, 34, 1};
+		int[] arr = {-1,-13, -30, -10, -34, -2,-3};
 		
 		find2ndMax(arr);
 
@@ -31,11 +32,11 @@ public class SecondMaxInArray {
 				 _1stMaxNum=arr[i];
 				
 			 }
-			
+			 if(  arr[i] > _2ndMaxNum  & arr[i]!=_1stMaxNum)
+				 _2ndMaxNum= arr[i];
 		 }
 		 
-		 if(arr[arr.length-1]>_2ndMaxNum)
-			 _2ndMaxNum= arr[arr.length-1];
+		 
 		 
 		 System.out.println("2nd Max element in array " +_2ndMaxNum);
 		 
