7b90360954e0c5a244de20a16a1a85a14f7e3168
sa3047 <swapnil_aher@yahoo.co.in>
2015-03-26 11:53:00 -0400

    description added

--- FullTimePractice/src/com/java/javaconcepts/ConstructorConcept.java
+++ FullTimePractice/src/com/java/javaconcepts/ConstructorConcept.java
@@ -1,5 +1,8 @@
 package com.java.javaconcepts;
-
+/*
+ *1)JVM will add Default constructor in your class if you do not  provide any constructor
+ *2) if this() is called in any constructor then call to super() is omitted by JVM
+ */
 public class ConstructorConcept {
 
 	public static void main(String[] args) {
@@ -12,11 +15,16 @@ public class ConstructorConcept {
 class Car
 {
 	int price;
+	
 	Car(int p)
 	{
 		this.price = p;
 	}
 	
+	int getPrice()
+	{
+		return this.price;
+	}
 	
 }
 
